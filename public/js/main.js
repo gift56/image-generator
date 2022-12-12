3484d45657604691b7c4bbbdd57304783878fe0e
gift56 <efeasiughu@gmail.com>
2022-12-12 10:18:27 -0800

    working on script

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const onSubmit = (e) => {
   e.preventDefault();
-  
+
+  const prompt = document.querySelector("#prompt").value;
+  const size = document.querySelector("#size").value;
 };
 
 document.querySelector("#image-form").addEventListener("submit", onSubmit);
