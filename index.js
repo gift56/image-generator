41e3094a113814c68c1c9131ac7f49c546f5281c
gift56 <efeasiughu@gmail.com>
2022-12-12 09:56:24 -0800

    done with backend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static middleware
+// Set static folder
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
