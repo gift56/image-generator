4bdc2e86bcdcd3067389e4e59079c5230c9c7092
gift56 <efeasiughu@gmail.com>
2022-12-12 10:26:31 -0800

    working on script

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,38 @@ const onSubmit = (e) => {
   generateImageRequest(prompt, size);
 };
 
-const generateImageRequest = async (prompt, size) => {};
+const generateImageRequest = async (prompt, size) => {
+  try {
+    showSpinner();
+
+    const response = await fetch("/openai/generateimage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("That image could not be generated");
+    }
+
+    const data = await response.json();
+    // console.log(data);
+
+    const imageUrl = data.data;
+
+    document.querySelector("#image").src = imageUrl;
+
+    removeSpinner();
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
+};
 
 const showSpinner = () => {
   document.querySelector(".spinner").classList.add("show");
